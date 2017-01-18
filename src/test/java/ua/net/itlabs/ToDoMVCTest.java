4cfe2b748c8c63db25ca6626460447eb3fd7de60
Inna <inna@Innas-MacBook-Pro-2.local>
2017-01-18 21:37:17 +0100

    second commit

--- src/test/java/ua/net/itlabs/ToDoMVCTest.java
+++ src/test/java/ua/net/itlabs/ToDoMVCTest.java
@@ -1,32 +1,58 @@
 package ua.net.itlabs;
 
-import static com.codeborne.selenide.CollectionCondition.exactTexts;
-import static com.codeborne.selenide.CollectionCondition.size;
-import com.codeborne.selenide.Configuration;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import org.junit.Test;
 
+import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
+/**
+ * Created by inna on 1/15/17.
+ */
 
 public class ToDoMVCTest {
 
-
     @Test
-    public void testCreateTask(){
+    public void tasksCommonFlow(){
+
+        open("https://todomvc4tasj.herokuapp.com/");
+        $("#new-todo").setValue("create task1").pressEnter();
+        $("#new-todo").setValue("create task2").pressEnter();
+        $("#new-todo").setValue("create task3").pressEnter();
+        $("#new-todo").setValue("create task4").pressEnter();
+        $$("#todo-list li").shouldHave(exactTexts("create task1", "create task2", "create task3", "create task4"));
+
+        $("#todo-list li:nth-child(2)").hover();
+        $("#todo-list li:nth-child(2) .destroy").click();
+        $$("#todo-list li").shouldHave(exactTexts("create task1", "create task3", "create task4"));
+
+        $("#todo-list li:nth-child(3) .toggle").click();
+        $$("#todo-list .completed").shouldHave(texts("create task4"));
+
+        $("#clear-completed").click();
+        //$$("#todo-list .completed").shouldBe(empty);
+        $$("todo-list li").filterBy(cssClass("completed")).shouldBe(empty);
+
+        $("#toggle-all").click();
+        $$("#todo-list li.completed").shouldHaveSize(2);
+
+        $("#clear-completed").click();
+        $$("#todo-list li").shouldBe(empty);
 
 
-        /*System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        Configuration.browser = "chrome";*/
 
-        open("http://todomvc.com/examples/troopjs_require/#/");
-        $(".new-todo").setValue("get grand piano").pressEnter();
-        $(".new-todo").setValue("be happy").pressEnter();
 
-        $$(".todo-list li").shouldHave(exactTexts("get grand piano", "be happy"));
+
 
     }
+
 
 
 }
