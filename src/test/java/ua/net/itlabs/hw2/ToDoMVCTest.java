0d469df6895ca2bdafb24d6bb5704057762279a6
CoffeeInka <inna.shevchenko@gmail.com>
2017-01-29 18:01:11 +0100

    hw2. Edit 1

--- src/test/java/ua/net/itlabs/hw2/ToDoMVCTest.java
+++ src/test/java/ua/net/itlabs/hw2/ToDoMVCTest.java
@@ -1,44 +1,52 @@
 package ua.net.itlabs.hw2;
 
 import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.Test;
 
 import static com.codeborne.selenide.CollectionCondition.empty;
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ToDoMVCTest {
 
     @Test
-    public void e2eSmokeTest() {
+    public void tasksLifeCycle() {
 
         open("https://todomvc4tasj.herokuapp.com/");
 
-        add("1");
-        delete("1");
-        tasks.shouldBe(empty);
+        add("1", "2");
+        cancelRename("1", "1 edit canceled");
+        toggle("1");
+        assertTasksAre("1", "2");
 
-        add("2");
-        cancelEditTask("2", "3");
-        editTask("2", "3");
+        switchFilterToActive();
+        assertTasksAre("", "2");
+        assertVisibleTasksAre("2");
 
-        complete("3");
-        assertTaskIsVisible("3");
+        applyRename("2", "2 edited");
+        toggleAll();
+        assertTasksAre("", "");
+        assertVisibleTasksAre();
 
-        switchFilterTo("Completed");
-        //here I could avoid reopen and complete terminology and use "toggle" instead, but this way readability is better
-        reopen("3");
-        assertTaskDisappears("3");
+        switchFilterToCompleted();
+        assertVisibleTasksAre("1", "2 edited");
 
-        switchFilterTo("Active");
-        assertTaskIsVisible("3");
+        toggle("1");
+        assertTasksAre("", "2 edited");
+        assertVisibleTasksAre("2 edited");
 
-        completeAll();
-        assertItemsLeft("0");
         clearCompleted();
-        tasks.shouldBe(empty);
+        assertTasksAre("");
+        assertVisibleTasksAre();
+
+        assertItemsLeft(1);
+
+        switchFilterToAll();
+        delete("1");
+        assertTasksAreEmpty();
     }
 
 
@@ -56,48 +64,61 @@ public class ToDoMVCTest {
         tasks.find(exactText(taskText)).hover().$(".destroy").click();
     }
 
-    private void complete(String taskText) {
+    private void toggle(String taskText) {
         tasks.findBy(exactText(taskText)).find(".toggle").click();
     }
 
-    private void reopen(String taskText) {
-        //this method implies task is already in completed state;
-        complete(taskText);
-    }
-
-    private void completeAll() {
+    private void toggleAll() {
         $("#toggle-all").click();
     }
 
-    private void assertItemsLeft(String itemsLeft) {
-        $("#todo-count").equals(itemsLeft);
+    private boolean assertItemsLeft(int count) {
+        $("#todo-count").equals(count);
+        return true;
     }
 
     private void clearCompleted() {
         $("#clear-completed").click();
     }
 
-    private void cancelEditTask(String initText, String newText) {
-        tasks.findBy(exactText(initText)).doubleClick();
-        tasks.findBy(cssClass("editing")).find(".edit").setValue(newText).pressEscape();
+    private SelenideElement setNewTaskName(String oldTaskText, String newTaskText) {
+        tasks.findBy(exactText(oldTaskText)).doubleClick();
+        return tasks.findBy(cssClass("editing")).find(".edit").setValue(newTaskText);
     }
 
-    private void editTask(String initText, String newText) {
-        tasks.findBy(exactText(initText)).click();
-        tasks.findBy(exactText(initText)).doubleClick();
-        tasks.findBy(cssClass("editing")).find(".edit").setValue(newText).pressEnter();
+    private void applyRename(String oldTaskText, String newTaskText) {
+        setNewTaskName(oldTaskText, newTaskText).pressEnter();
     }
 
-    private void assertTaskIsVisible(String taskText) {
-        tasks.findBy(exactText(taskText)).is(visible);
+    private void cancelRename(String oldTaskText, String newTaskText) {
+        setNewTaskName(oldTaskText, newTaskText).pressEscape();
     }
 
-    private void switchFilterTo(String filterName) {
-        filters.findBy(exactText(filterName)).click();
+    private void switchFilterToCompleted() {
+        filters.findBy(exactText("Completed")).click();
     }
 
-    private void assertTaskDisappears(String taskText) {
-        tasks.findBy(exactText(taskText)).should(disappear);
+    private void switchFilterToActive() {
+        filters.findBy(exactText("Active")).click();
+    }
+
+    private void switchFilterToAll() {
+        filters.findBy(exactText("All")).click();
+    }
+
+    private void assertTasksAreEmpty() {
+        tasks.shouldBe(empty);
+    }
+
+    private void assertTasksAre(String... taskTexts) {
+        tasks.shouldHave(texts(taskTexts));
+    }
+
+    private void assertVisibleTasksAre(String... taskText) {
+        for (String text : taskText) {
+            tasks.filterBy(visible).shouldHave(exactTexts(taskText));
+        }
+
     }
 
 }
