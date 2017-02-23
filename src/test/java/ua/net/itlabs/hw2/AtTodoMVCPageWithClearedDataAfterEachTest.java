ed2456e516f43e444612a488ee4a623f5bc62197
CoffeeInka <inna.shevchenko@gmail.com>
2017-02-23 23:21:56 +0100

    Parent classes added

---
+++ src/test/java/ua/net/itlabs/hw2/AtTodoMVCPageWithClearedDataAfterEachTest.java
@@ -0,0 +1,28 @@
+package ua.net.itlabs.hw2;
+
+import com.codeborne.selenide.Screenshots;
+import com.google.common.io.Files;
+import org.junit.After;
+import org.junit.Before;
+import ru.yandex.qatools.allure.annotations.Attachment;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AtTodoMVCPageWithClearedDataAfterEachTest extends BaseTest {
+
+    @Before
+    public void openPage() {
+        open("https://todomvc4tasj.herokuapp.com/");
+    }
+
+    @After
+    public void clearData() {
+        executeJavaScript("localStorage.clear()");
+    }
+
+}
